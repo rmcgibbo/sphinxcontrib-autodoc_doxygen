b7572b70a344aa275bd0671bd4002ff5a2c4ea9d
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-19 19:48:01 -0800

    Deploy

--- tests/deploy-ghpages.sh
+++ tests/deploy-ghpages.sh
@@ -5,7 +5,7 @@ mkdir out;
  git init
  git config user.name "Travis-CI"
  git config user.email "noreply@travis-ci.org"
- cp $TRAVIS_BUILD_DIR/deploy-html/* .
+ cp -r $TRAVIS_BUILD_DIR/deploy-html/* .
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1

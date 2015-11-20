99bfec3ccdee705a7fbe101a67f2f66dbd46bc58
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-19 21:10:36 -0800

    try to preserve history

--- tests/deploy-ghpages.sh
+++ tests/deploy-ghpages.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
-GH_USER_NAME="$(git --no-pager show -s --format='%an' HEAD)"
-GH_USER_EMAIL="$(git --no-pager show -s --format='%ae' HEAD)"
+#!/bin/bash -e
+git config user.email "$(git --no-pager show -s --format='%ae' HEAD)"
+git config user.name "$(git --no-pager show -s --format='%an' HEAD)"
 COMMIT_ID="$(git rev-parse --short HEAD)"
 
-(cd $TRAVIS_BUILD_DIR/deploy-html && \
- git init && \
- git config user.name $GH_USER_NAME && \
- git config user.email $GH_USER_EMAIL && \
- git add . && \
- git commit -m "Travis-CI deploy ${COMMIT_ID} to gh-pages" && \
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+TEMPDIR=$(mktemp -d)
+trap "rm -rf ${TEMPDIR}" EXIT
+
+cd $TEMPDIR
+git clone -b gh-pages --single-branch "https://${GH_REF}" gh-pages
+cp -rf $TRAVIS_BUILD_DIR/deploy-html/* gh-pages/
+cd gh-pages
+git add .
+git commit -m "Travis-CI deploy ${COMMIT_ID} to gh-pages" && \
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+

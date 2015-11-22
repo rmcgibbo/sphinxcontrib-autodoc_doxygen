9e73a2514b6337e518cf828948be31024454ffd2
Robert McGibbon <rmcgibbo@gmail.com>
2015-11-21 17:07:35 -0800

    use pbr for setup.py

--- setup.py
+++ setup.py
@@ -1,51 +1,8 @@
-import io
-from setuptools import setup, find_packages
+#!/usr/bin/env python
 
-
-def readfile(filename):
-    with io.open(filename, encoding="utf-8") as stream:
-        return stream.read().splitlines()
-
+from setuptools import setup
 
 setup(
-    name='sphinxcontrib-autodoc_doxygen',
-    url='https://github.com/rmcgibbo/sphinxcontrib-autodoc_doxygen',
-    download_url='https://pypi.python.org/pypi/sphinxcontrib-autodoc_doxygen',
-    license="MIT",
-    author="Robert T. McGibbon",
-    author_email="rmcgibbo@gmail.com",
-    description="Doxygen / Sphinx bridge, with autodoc and autosummary",
-    long_description='\n'.join(readfile("README.rst")[3:]),
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Documentation',
-        'Topic :: Utilities',
-    ],
-    platforms='any',
-    packages=find_packages(),
-    namespace_packages=['sphinxcontrib'],
-    # package_data={'sphinxcontrib.autodoc_doxygen.autosummary': ['templates/*']},
-    include_package_data=True,
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    install_requires=[
-        'sphinx==1.3.1',
-        'docutils>=0.11',
-        'jinja2>=2.3',
-        'lxml',
-    ],
+    setup_requires=['pbr'],
+    pbr=True,
 )

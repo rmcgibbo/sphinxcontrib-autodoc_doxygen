f3dd4d1810c8d283117d377f36f6edc574ea5488
Robert T. McGibbon <rmcgibbo@gmail.com>
2015-11-21 20:27:05 -0800

    Remove objects.inv

--- examples/deploy-ghpages.sh
+++ examples/deploy-ghpages.sh
@@ -11,7 +11,7 @@ git clone -b gh-pages --single-branch "https://${GH_REF}" gh-pages
 cd gh-pages
 rm -f $(git ls-files)
 cp -rf $TRAVIS_BUILD_DIR/deploy-html/* .
-echo '**/objects.inv' > .gitignore
+find . -name objects.inv -delete
 git add -A .
 git rm -q $(git ls-files --deleted) || true
 git commit -m "Travis-CI deploy ${COMMIT_ID} to gh-pages"
